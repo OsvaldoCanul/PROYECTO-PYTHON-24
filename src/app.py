c92f326941ea0fcac1f8be8ceff65482d5783b47
OsvaldoCanul <canul121812@gmail.com>
2024-02-22 12:31:54 -0600

    Commit 1

---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template #Es para importar flask
+
+app = Flask(__name__) 
+
+@app.route('/')
+def Hola(): 
+    return '<h1>Bienvenido al sitio Web de Osvaldo Canul</h1>' 
+@app.route('/plantilla')
+def plantilla():
+    data={
+        'titulo':'Página plantilla',
+        'mensaje':'Hola'
+    } 
+    return render_template('pagina1.html',data=data) 
+app.run(debug=True) 
